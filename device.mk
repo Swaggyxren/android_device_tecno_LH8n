eaf0ba20754cafe149436d23ef4ef26c9e8d4e93
nazephyrus <naden.irsyad01@gmail.com>
2026-03-23 09:47:41 +0700

    LH8n : copy media folder to vendor

--- device.mk
+++ device.mk
@@ -172,6 +172,7 @@ PRODUCT_PACKAGES += \
 
 # Media
 PRODUCT_COPY_FILES += \
+	$(call find-copy-subdir-files,*,$(LOCAL_PATH)/configs/media,$(TARGET_COPY_OUT_VENDOR)/etc)
     $(call find-copy-subdir-files,*,$(LOCAL_PATH)/configs/seccomp,$(TARGET_COPY_OUT_VENDOR)/etc/seccomp_policy) \
 
 # NFC
