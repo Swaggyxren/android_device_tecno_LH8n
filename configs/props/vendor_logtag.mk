60c14da48cf3aeea1f0adcb6b39f7dacc498a70e
nazephyrus <naden.irsyad01@gmail.com>
2026-03-25 09:27:01 +0700

    LH8n : quiet BLASTBufferQueue
    this man can literally take 100+ mb log

--- configs/props/vendor_logtag.mk
+++ configs/props/vendor_logtag.mk
@@ -261,4 +261,5 @@ PRODUCT_VENDOR_PROPERTIES += \
     persist.log.tag.hwcomposer=$(VENDOR_LOG_LEVEL) \
     persist.log.tag.GPUAUX=$(VENDOR_LOG_LEVEL) \
     persist.log.tag.HWUI=$(VENDOR_LOG_LEVEL) \
-    persist.log.tag.FrameTracker=$(VENDOR_LOG_LEVEL)
+	persist.log.tag.FrameTracker=$(VENDOR_LOG_LEVEL) \
+	persist.log.tag.BLASTBufferQueue=$(VENDOR_LOG_LEVEL)
