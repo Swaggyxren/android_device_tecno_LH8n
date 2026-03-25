52150c0f09ed0dcad18328861760b8cfd5a76590
nazephyrus <naden.irsyad01@gmail.com>
2026-03-25 21:37:08 +0700

    rubyx: overlay: Enable call recording

--- device.mk
+++ device.mk
@@ -213,8 +213,8 @@ PRODUCT_ENFORCE_RRO_TARGETS := *
 	
 # Lineage-Specific Overlays
 PRODUCT_PACKAGES += \
-    ApertureOverlay
-	
+    ApertureOverlay \
+	LineageDialer
 # Permissions
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.audio.low_latency.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.audio.low_latency.xml \
