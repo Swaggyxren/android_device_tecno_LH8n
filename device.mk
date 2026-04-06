84e0d8fb0999548b44bacc5e507a2c9d3fc4f13d
nazephyrus <naden.irsyad01@gmail.com>
2026-04-06 18:51:44 +0700

    LH8n : Inherit MSSI common overlays

--- device.mk
+++ device.mk
@@ -215,15 +215,14 @@ PRODUCT_COPY_FILES += \
 	$(call find-copy-subdir-files,*,$(LOCAL_PATH)/configs/nfc,$(TARGET_COPY_OUT_VENDOR)/etc)
 	
 # Overlays
+$(call inherit-product, hardware/mediatek/overlay/mssi.mk)
 PRODUCT_PACKAGES += \
     CarrierConfigOverlay_LH8n \
     FrameworksResOverlay_LH8n \
     SettingsResOverlay_LH8n \
 	NfcResOverlay_LH8n \
     SystemUIOverlay_LH8n \
-	Launcher3DeviceOverlay_LH8n \
-    TetheringResOverlay_LH8n \
-    WifiResOverlay_LH8n
+	Launcher3DeviceOverlay_LH8n
 
 PRODUCT_PACKAGES += \
     NcmTetheringOverlay
