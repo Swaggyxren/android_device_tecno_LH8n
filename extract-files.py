9fbcfc5f80e052ed0d5d01904fcaeb7824ed6be1
Nolen Johnson <johnsonnolen@gmail.com>
2026-08-30 21:34:19 +0800

    LH8n: make libformatter MTK specific
    * Avoid collisions with Car libraries.
    
    Change-Id: I602b1d65fdf672244a91a3f7c4a71470beae7ea4
    Signed-off-by: Shirayuki39 <lorddemecrius83@proton.me>

--- extract-files.py
+++ extract-files.py
@@ -116,7 +116,9 @@ blob_fixups: blob_fixups_user_type = {
         .replace_needed('libtinyxml2.so', 'libtinyxml2-v34.so'),
         'system_ext/bin/kpoc_charger': blob_fixup()
         .add_needed('libbinder_shim.so'),
-}  # fmt: skip
+    ('vendor/lib64/libcodec2_mtk_vdec.so', 'vendor/lib64/libcodec2_mtk_venc.so'): blob_fixup()
+        .replace_needed('libformatter.so', 'libformatter_mtk.so'),
+}
 
 module = ExtractUtilsModule(
     'LH8n',
